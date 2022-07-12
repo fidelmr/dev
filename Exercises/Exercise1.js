0bbb706abfdd9c8a1ccd4836f68c6a4298f97a40
fidelmr <fde_los_riscos@uoc.edu>
2022-07-12 18:40:03 +0200

    reescribiendo-ejemplos

--- Exercises/Exercise1.js
+++ Exercises/Exercise1.js
@@ -3,3 +3,111 @@ console.log ('holJS');
 console.log (document.getElementById('saludo'));
 
 
+let a = 5;
+let b =6;
+
+console.log (a);
+
+a = 15;
+
+console.log (a);
+
+let c=1; let d=2; let e=3;
+console.log ((c) + (d) +( e)) ;
+console.log (c,d,e);
+
+console.log ('el valor de c es: '  + c +   ', el valor de d es : ' +d);
+
+function example() {
+    console.log (c);
+    let f= 5;
+    console.log ( f );
+  }
+a=25;
+
+a ++;
+
+console.log (a);
+
+g=30
+
+g++;
+
+console.log ('el valor de g es: ' + g );
+
+console.log ( 'la comparación es: ' + (a == g) );
+
+let f = (g);
+
+console.log ('la comparación es: ' + ( f==g));
+
+g != f;
+
+console.log ('el resultado de comparar si g no es igual a f es:' + (g!=f));
+
+let s= ('hola');
+let r= ('holas');
+
+console.log ( s==r );
+
+console.log (s===r);
+
+/*let t= '4';
+let v= '4';*/
+let t= 4;
+let v= 4;
+
+console.log (t===v);
+
+let u= t+v;
+
+console.log (u);
+/*
+CONCATENAR TEXTOS DEFINIDOS EN VARIABLES   */
+
+t= 'Me llamo';
+u= '  Fidel';
+
+console.log (t+u);
+
+/*PARSEINT parseInt*/
+
+t='8';
+u=10;
+
+y= t+u;
+
+console.log (y);
+
+y= +t+10;
+
+console.log (y);
+
+y= parseInt (t)+ parseInt (u);
+
+console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
