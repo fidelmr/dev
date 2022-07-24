6b0ea17a096ef77d2212710b0fe21fe91f810f8c
fidelmr <fde_los_riscos@uoc.edu>
2022-07-24 21:07:19 +0200

    reto1-test2

--- Exercises/Reto1.js
+++ Exercises/Reto1.js
@@ -18,11 +18,37 @@ console.log(result);
 else {console.log('please try again')}*/
 
 
-prompt (operator1);
+/*prompt (operator1);
 prompt (operator2);
 if ( operator1 || operator2 < 0 && operator1 || operator2 > 51 ) {console.log
 ('continue');}
 else {console.log ('please enter a value between 1 and 50')
+}*/
+
+if (operator1 > 1 && operator1 < 51 )
+{console.log('continue')}
+else{console.log('try again')};
+
+alert ('introduce operator');
+
+function fidel (){
+var name = prompt('team name');
+var color = prompt('team color');
+fidel.name = name;
+fidel.color = color;
 }
 
- 
+fidel();
+alert (fidel.name + ',' + fidel.color);
+
+let nombre;
+
+nombre = prompt("Introduce tu nombre");
+
+if (nombre != null){
+	document.write("Bienvenido/a a mi sitio web, " + nombre + ".");
+} else {
+	alert("No has introducido nada. Recarga la página para intentarlo de nuevo.");
+}
+
+
