a40c78fe55fb2f46e341d02883dc5a99ffa58bec
fidelmr <fde_los_riscos@uoc.edu>
2022-07-24 13:30:33 +0200

    REto1-test

--- Exercises/Reto1.js
+++ Exercises/Reto1.js
@@ -1,1 +1,28 @@
 console.log('JavaScript Funciona')
+
+var operator1 = 50;
+var operator2 = 1;
+
+function explainVar(){
+    operator1 = 50;
+    console.log(operator1);
+}
+
+explainVar();
+
+var result = (operator1 + operator2);
+console.log(result);
+
+/*if (operator1 <0 &&  operator1 >51 ) 
+{console.log ('ok continue')}
+else {console.log('please try again')}*/
+
+
+prompt (operator1);
+prompt (operator2);
+if ( operator1 || operator2 < 0 && operator1 || operator2 > 51 ) {console.log
+('continue');}
+else {console.log ('please enter a value between 1 and 50')
+}
+
+ 
